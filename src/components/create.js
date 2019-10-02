7baa92ba59c054fdcb5ca10dd8e398d8ea7be489
SHAUN DALY - STUDENT <G00286445@gmit.ie>
2019-10-02 10:36:48 +0100

    Lab work

---
+++ src/components/create.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Create extends React.Component {
+
+  render() { // Component has to have render method
+    return (
+      <div>
+        <h1>Hello from Create component</h1>
+      </div>
+    );
+  }
+}
+
+export default Create;
---
+++ src/components/movieItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MovieItem extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from MovieItem component</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
